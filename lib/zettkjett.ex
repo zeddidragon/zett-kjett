8b0b0fd8d7fcb43a46856121cc350dec37620a01
tony <tony>
2017-02-27 21:18:25 +0100

    Test more better

--- lib/zettkjett.ex
+++ lib/zettkjett.ex
@@ -1,6 +1,20 @@
 defmodule ZettKjett do
+  alias ZettKjett.Config
   def start _type, _args do
     IO.puts "Welcome to ZettKjett"
-    ZettKjett.Config.start_link
+    Config.start_link
+    if Mix.env != :test do
+      connect()
+    end
+    {:ok, self()}
+  end
+
+  def connect do
+    for {protocol, config} <- Config.get do
+      if config[:enabled] do
+        IO.puts "Connecting to #{protocol}..."
+        ZettKjett.Protocol.start_link protocol
+      end
+    end
   end
 end
